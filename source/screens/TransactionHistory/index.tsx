7c68b64909bf5fb3c167f250263349fa69869684
Noah <noah@simajji.com>
2024-09-14 14:05:31 +0800

    Added isMasked feature on TransactionHistory screen

--- source/screens/TransactionHistory/index.tsx
+++ source/screens/TransactionHistory/index.tsx
@@ -1,12 +1,15 @@
-import React, {useEffect} from 'react';
+import React, {useState} from 'react';
 
 import {
+  Alert,
   FlatList,
   SafeAreaView,
   Text,
   TouchableOpacity,
   View,
 } from 'react-native';
+
+import ReactNativeBiometrics from 'react-native-biometrics';
 
 import {MockTransactionDataList} from '~MockData/MockTransactionDataList';
 
@@ -16,9 +19,32 @@ import {styles} from './styles';
 export default function TransactionHistory({
   navigation,
 }: TransactionHistoryProps) {
+  const [isMasked, setIsMasked] = useState(true);
+
+  const biometrics = new ReactNativeBiometrics({allowDeviceCredentials: true});
+
   // MARK: Events
   function onTransactionButtonPress(item: ITransactionList) {
     navigation.navigate('TransactionDetails', item);
+  }
+
+  async function onBiometricButtonPress() {
+    try {
+      // NOTE: If get biometric will not get again
+      if (!isMasked) return;
+
+      const {success, error} = await biometrics.simplePrompt({
+        promptMessage: 'Confirmation',
+      });
+
+      if (!success) {
+        throw error;
+      }
+
+      setIsMasked(false);
+    } catch (error) {
+      Alert.alert('Something went wrong', error);
+    }
   }
 
   // MARK: Render Methods
@@ -33,13 +59,19 @@ export default function TransactionHistory({
           <Text style={styles.transactionText} numberOfLines={1}>
             {description}
           </Text>
-          <Text style={styles.transactionTextDeduct}>{`-RM ${amount}`}</Text>
+          <Text
+            style={styles.transactionTextDeduct}
+            onPress={onBiometricButtonPress}>{`-RM ${
+            isMasked ? '****' : amount
+          }`}</Text>
         </View>
         <View style={styles.transactionItemRow}>
           <Text style={styles.transactionDate}>{date}</Text>
           <Text style={styles.transactionType}>{type}</Text>
         </View>
-        <Text>{`Payment Id: ${id}`}</Text>
+        <Text onPress={onBiometricButtonPress}>{`Payment Id: ${
+          isMasked ? '****' : id
+        }`}</Text>
       </TouchableOpacity>
     );
   }
