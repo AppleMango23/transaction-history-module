4ced0ab8977ac71223f4bb3d2599f5e9fa6c347e
Noah <noah@simajji.com>
2024-09-14 12:41:06 +0800

    Added biometric prompting and navigate to the transactionHistory screen

--- source/screens/Login/styles.ts
+++ source/screens/Login/styles.ts
@@ -1,3 +1,29 @@
 import {StyleSheet} from 'react-native';
 
-export const styles = StyleSheet.create({});
+import Colors from '~helpers/Colors';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignContent: 'center',
+  },
+  title: {
+    fontSize: 25,
+  },
+  description: {
+    fontSize: 15,
+    textAlign: 'center',
+    marginVertical: 20,
+  },
+  biometricButton: {
+    backgroundColor: Colors.THEME_1,
+    borderRadius: 15,
+    padding: 10,
+  },
+  biometricButtonText: {
+    color: Colors.WHITE,
+    fontSize: 18,
+  },
+});
