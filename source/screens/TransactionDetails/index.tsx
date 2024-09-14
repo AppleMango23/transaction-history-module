615612730a38e8b0eeb78025a9ebd2f5e291c451
Noah <noah@simajji.com>
2024-09-14 13:35:48 +0800

    Added TransactionDetails show out infomation

--- source/screens/TransactionDetails/index.tsx
+++ source/screens/TransactionDetails/index.tsx
@@ -1,19 +1,20 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 
-import {SafeAreaView, Text} from 'react-native';
+import {Text, View} from 'react-native';
 
 import {styles} from './styles';
 import {TransactionDetailsProps} from './types';
 
-export default function TransactionDetails({
-  navigation,
-  route,
-}: TransactionDetailsProps) {
-  useEffect(() => {}, []);
+export default function TransactionDetails({route}: TransactionDetailsProps) {
+  const {id, amount, date, description, type} = route.params;
 
   return (
-    <SafeAreaView>
-      <Text>Welcome</Text>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Text style={styles.textStandard}>{`Payment Id: ${id}`}</Text>
+      <Text style={styles.textStandard}>{`Date: ${date}`}</Text>
+      <Text style={styles.textStandard}>{`Description: ${description}`}</Text>
+      <Text style={styles.textStandard}>{`Type: ${type}`}</Text>
+      <Text style={styles.textStandard}>{`Amount: ${amount}`}</Text>
+    </View>
   );
 }
