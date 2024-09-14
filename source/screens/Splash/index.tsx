5b32936fc49a3ecac9f2d80404b9e3c21711e54c
Noah <noah@simajji.com>
2024-09-14 11:33:13 +0800

    Added splash screen

---
+++ source/screens/Splash/index.tsx
@@ -0,0 +1,20 @@
+import React, {useEffect} from 'react';
+
+import {SafeAreaView, Text} from 'react-native';
+
+import {styles} from './styles';
+import {SplashProps} from './types';
+
+export default function Splash({navigation, route}: SplashProps) {
+  useEffect(() => {
+    // setTimeout(() => {
+    //   navigation.navigate("Login")
+    // }, 1500)
+  }, []);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Welcome</Text>
+    </SafeAreaView>
+  );
+}
